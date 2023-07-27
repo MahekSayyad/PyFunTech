d711e0c8637c3662daa57d6ac125cc774b547582
MahekSayyad <140725227+MahekSayyad@users.noreply.github.com>
2023-07-27 19:29:11 +0530

    Add files via upload
    📸 Screenshot App with GUI 🚀
    
    Capture your screen effortlessly using this Python-based Screenshot App with GUI. With just a click, grab screenshots and save them instantly. An easy-to-use tool to streamline your screen capture process and enhance productivity. Start taking snapshots now!

---
+++ screenshots_app.py
@@ -0,0 +1,28 @@
+import time
+import pyautogui 
+import tkinter as tk
+
+def screenshot():  
+    name = int(round(time.time() * 1000))
+    name = 'C:\\Users\\Admin\\Desktop\\projects\\screenshots data\\{}.png'.format(name)
+    time.sleep(5)
+    img = pyautogui.screenshot(name)
+    img.show()
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(
+    frame,
+    text="Take Screenshot",
+    command=screenshot)
+    
+button.pack(side=tk.LEFT) 
+close = tk.Button(
+    frame,
+    text="Quit",
+    command=quit)
+close.pack(side=tk.LEFT)  
+
+root.mainloop()
